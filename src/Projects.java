bb858a4e2b8749760643e130ac3e1d076688e69b
wa_nu <waqas@itinsync.com>
2022-09-09 17:18:50 +0500

    Initial commit

---
+++ src/Projects.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Projects {
+    public static void main(String[] args) {
+        System.out.print("Enter your Number");
+        Scanner input = new Scanner(System.in) ;
+        System.out.println(input.hasNextInt());
+    }
+}
