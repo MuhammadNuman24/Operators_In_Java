bd95c8c435e8e6316ad4d07af95e22342dcb7d3c
wa_nu <waqas@itinsync.com>
2022-09-09 17:32:12 +0500

    Perform operation with operators

--- src/Operators.java
+++ src/Operators.java
@@ -1,5 +1,24 @@
 public class Operators {
     public static void main(String[] args) {
 
+        // Arithmetic Operators +, - ,  / , *
+         int a = 10;
+         int b = 14-a;
+        System.out.println(b);
+        int c = 14 * b;
+        System.out.println(c);
+        // Assignment Operators = , +=
+        int d = c + 2;
+        d += 9;
+        System.out.println(d);
+        // Comparison Operator  ==, > ,< ,>= , <=
+        System.out.println(d==c);
+        System.out.println(d>c);
+        System.out.println(d<=b);
+         // Logical Operators  && , ! , ||
+        System.out.println(68>58 && 68>78);
+        System.out.println(68>58 || 68>78);
+        System.out.println( 5 != 54);
+
     }
 }
